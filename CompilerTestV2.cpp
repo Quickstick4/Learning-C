ae90f2850618ac563144bfb44175c9856d1da5ee
Gregory Keenan <gfkeenan@Gregorys-MacBook-Pro.local>
2020-05-18 21:26:16 +0100

    New Compiler

--- CompilerTestV2.cpp
+++ CompilerTestV2.cpp
@@ -7,45 +7,106 @@
 //Read lines of text, sort them, and print the results to the stadard output.
 //If the command lune names a file, read from that file. otherwise, read from the standard input. The tinter input is stored in memory, so don't try the with input files that exceed available RAM
 
+//Program from Learning C++ a programers introduction
+//Sort the standard input alpabetically
+
 #include <algorithm>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <locale>
 #include <ostream>
 #include <string>
 #include <vector>
 
-void read(std::istream& in, std::vector<std::string>& text)
+//only typing this out to test the compiler. Nothing else. No learning to be gained from this. Comments not typed out either.
+template<class Ch, class Tr, class OutIter>
+
+void read (std::basic_istream<Ch,Tr>& in, OutIter iter)
 {
-    std::string line;
+    std::basic_string<Ch,Tr> line;
     while (std::getline(in, line))
-        text.push_back(line);
+    {
+        *iter = line;
+        ++iter;
+    }
 }
 
-int main(int argc, char* argv[])
+//sorter function object
+template<typename Ch>
+class sorter
 {
-    //Part 1. Read the entire input into text. If the command line names a file, read that file. Otherwise read the standard input.
+    public:
 
-    std::vector<std::string> text;  //store the lines of text here
-    if (argc <2)
-        read(std::cin, text);
+    sorter(Ch const* locname) :
+        loc_(std::locale(locname)),
+        collate_(std::use_facet<std::collate<Ch> >(loc_))
+    {}
+
+    sorter() :
+        loc_(std::locale()),
+        collate_(std::use_facet<std::collate<Ch> >(loc_))
+    {}
+
+    template<typename Tr>
+    bool operator()(const std::basic_string<Ch,Tr>& lhs,
+                    const std::basic_string<Ch,Tr>& rhs)
+                    {
+                        return collate_.compare(lhs.data(), lhs.data()+lhs.size(), 
+                        rhs.data(), rhs.data()+rhs.size()) < 0;
+                    }
+
+private:
+    std::locale loc_;
+    const std::collate<Ch>& collate_;
+
+
+};
+
+template<typename Ch>
+sorter<Ch> make_sorter(Ch const * name)
+{
+    return sorter<Ch>(name);
+}
+
+//Main Program
+int main(int argc, char* argv[])
+try
+{
+    std::cin.exceptions(std::ios_base::badbit);
+
+    std::vector<std::string> text;
+
+    if (argc < 2)
+        read(std::cin, std::back_inserter(text));
+    
     else
     {
-        std::ifstream in (argv[1]);
+        std::ifstream in(argv[1]);
         if (not in)
         {
             std::perror(argv[1]);
-            return EXIT_FAILURE;
         }
-        read(in, text);
+        read(in, std::back_inserter(text));
+
     }
 
-//Part 2. Sort the text
-std::sort(text.begin(), text.end());
+    std::sort(text.begin(), text.end(), make_sorter(argc >= 3 ? argv[2] : ""));
 
-//Part 3. Print the sorted text.
-std::copy(text.begin(), text.end(),
-    std::ostream_iterator<std::string>(std::cout, "\n"));
+    std::copy(text.begin(), text.end(),
+                    std::ostream_iterator<std::string>(std::cout, "\n"));
+    
 
-
+}
+catch(std::exception& ex)
+{
+    std::cerr << "Caught exception" << ex.what() << '\n';
+    std::cerr << "Terminating program.\n";
+    std::exit(EXIT_FAILURE);
+}
+catch (...)
+{
+    std::cerr << "Caught unknown exception type.\nTerminating program.\n";
+    std::exit(EXIT_FAILURE);
 }
