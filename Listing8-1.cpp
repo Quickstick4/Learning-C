e407671a18b187650060ea8e93b327b83f0af737
Gregory Keenan <gfkeenan4@gmail.com>
2020-05-29 17:28:06 +0100

    Updated with own solution to problem

--- Listing8-1.cpp
+++ Listing8-1.cpp
@@ -1,6 +1,7 @@
 
 //This is a new include from the Standard Library. 
 //TODO: List what this does
+//This header declares tools such as set width.
 #include <iomanip>
 
 #include <iostream>
@@ -8,9 +9,20 @@
 
 int main()
 {
-    std::cout << " N  N^2    N^3\n";
+    std::cout << "N    N^2  N^3\n";
     for (int i = 1; i != 21; ++i)
     {
         //Fill in the loop body here
+        if(i < 4)
+        {
+             std::cout << i << "    " << i*i << "    " << i*i*i << '\n';
+        } else if (i >= 4 && i < 10) {
+            std::cout << i << "    " << i*i << "   " << i*i*i << '\n';
+        } else if (i >= 10) {
+            std::cout << i << "   " << i*i << "  " << i*i*i << '\n';
+        }
+
+
+       
     }
 }
